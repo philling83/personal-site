fd4ba18941db406962e41b03a5b69fcd06c2b8d3
Philip <philipling83@gmail.com>
2021-03-01 16:54:31 -0500

    Edit path prefix to config

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/personal-site/`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Philip Ling',
   heading: 'Fullstack Software Developer',
   // social
