d1f5d96fed8ab5814b8bae1078c63fcabad52778
Philip <philipling83@gmail.com>
2021-03-02 18:51:51 -0500

    Add skills section

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const sections = [
   { id: 'top', name: 'Intro', icon: 'fa-home' },
   { id: 'portfolio', name: 'Portfolio', icon: 'fa-th' },
   { id: 'about', name: 'About Me', icon: 'fa-user' },
+  { id: 'skills', name: 'Skills', icon: 'fa-user' },
   { id: 'contact', name: 'Contact', icon: 'fa-envelope' },
 ];
 
